b0abe7a72e557e76eb5f13582ae1c72980f3f7ec
a-nemilov <30981373+a-nemilov@users.noreply.github.com>
2020-12-23 19:16:02 +0300

    Update script.js
    test strava data

--- script.js
+++ script.js
@@ -27,6 +27,31 @@ function log(msg) {
    }, 0);
 }
 
+// test strava data
+var StravaApiV3 = require('strava_api_v3');
+var defaultClient = StravaApiV3.ApiClient.instance;
+
+// Configure OAuth2 access token for authorization: strava_oauth
+var strava_oauth = defaultClient.authentications['strava_oauth'];
+strava_oauth.accessToken = "6e57d40b403333339ddf7ca6177881820a3892cf"
+
+var api = new StravaApiV3.ActivitiesApi()
+
+var id = 36416; // {Long} The identifier of the activity.
+
+var opts = { 
+  'includeAllEfforts': true // {Boolean} To include all segments efforts.
+};
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+api.getActivityById(id, opts, callback);
+
 log('Я не знаю, зачем ты смотришь JS debug консоль.', "color: green;");
 log('Но, видимо, тебе действительно нужно что-то важное. ', "color: green;");
 log('Такому человеку я готов дать свой номер телефона.', "color: green;");
